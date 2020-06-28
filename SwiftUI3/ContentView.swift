56a0a3046268aae563e3284d3fa932df61493fcc
Rohit Saini <sketchlearn7@gmail.com>
2020-06-28 18:50:22 +0530

    Code Updated

--- SwiftUI3/ContentView.swift
+++ SwiftUI3/ContentView.swift
@@ -9,8 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var todos:[ToDo] = [ToDo]()
+    @State var task:String = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            VStack{
+                TextField("add to do",text: $task,onCommit: addToDo)
+                    .padding()
+                List{
+                    ForEach(todos){todo in
+                        Text(todo.name)
+                    }
+                .onDelete(perform: deleteToDo)
+                }
+            }
+            .navigationBarTitle("To Do",displayMode: .inline)
+        }
+    }
+    
+    //Add to do Task
+    private func addToDo(){
+        todos.append(ToDo(name: task))
+        task = ""
+    }
+    //Remove to do Task
+    private func deleteToDo(at offsets:IndexSet){
+        todos.remove(atOffsets: offsets)
     }
 }
 
--- SwiftUI3/Model/Todo.swift
+++ SwiftUI3/Model/Todo.swift
@@ -7,3 +7,8 @@
 //
 
 import Foundation
+
+struct ToDo:Identifiable{
+    let id = UUID()
+    let name: String
+}
